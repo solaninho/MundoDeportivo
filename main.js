767762f5b6e0700e86cac572dd0ebeec9572cb94
EduardoGuerreroCa <18030547@itcelaya.edu.mx>
2021-06-08 21:55:08 -0500

    Scroll Reveal

--- main.js
+++ main.js
@@ -1,3 +1,8 @@
 document.querySelector('.menu-btn').addEventListener('click', () =>{
     document.querySelector('.nav-menu').classList.toggle('show');
 });
+
+ScrollReveal().reveal('.showcase');
+ScrollReveal().reveal('.news-cards', {deay: 500 });
+ScrollReveal().reveal('.cards-banner-one', {deay: 500 });
+ScrollReveal().reveal('.cards-banner-two', {deay: 500 });
